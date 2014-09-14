9ce144820b3e5ab0e145736642bf707b79e96c2f
rgopal <rgopal@gopals.us>
2014-09-14 17:09:26 -0400

    More calcs in Path.

---
+++ link/src/com/erudyo/satellite/GeoSatellite.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.erudyo.satellite;
+
+import static com.erudyo.satellite.Com.RE;
+
+/**
+ * Copyright (c) 2014 R. Gopal. All Rights Reserved.
+ * @author rgopal
+ */
+public class GeoSatellite extends Satellite {
+      
+    public GeoSatellite() {
+        
+        init();
+    }
+    public void init () {
+        setA(42164.2E3);     //semi major axis of GEO orbit
+        setV(3075E3);        // GEO satellite velocity
+        setR0(Com.RE/.178);       // height of GEO satellite
+        setPhi(0.0);         // latitude is zero
+        orbit = Com.Orbit.GEO;
+    }
+    public GeoSatellite(String n) {
+        super(n);
+        init();
+    }
+    public GeoSatellite (String n, String d, String s) {
+        super(n,d,s);
+        init();
+    }
+}
--- link/src/com/erudyo/satellite/Satellite.java
+++ link/src/com/erudyo/satellite/Satellite.java
@@ -6,25 +6,134 @@
 
 package com.erudyo.satellite;
 
+import com.codename1.util.MathUtil;
+
 /**
  * Copyright (c) 2014 R. Gopal. All Rights Reserved.
  * @author rgopal
  */
-public class Satellite {
-    private double L;   
-
+public class Satellite extends Entity {
+   
+    protected double phi;     // latitude
+    protected double lambda;  // longitude
+    protected double r;       // distance from center of Earth
+    protected double h;       // altitude of satellite from sub-satellite point
+    protected double a;       // semi major axis
+    protected double v;       // velocity
+    protected double R0;      // altitude
+    protected Com.Orbit orbit;
+  
+  
+    public Satellite () {
+        
+    }
+    public Satellite (String n) {
+        super(n);
+    }
+    public Satellite (String n, String d, String s) {
+        super(n,d,s);
+    }
+   
     /**
-     * @return the L
+     * @return the lambda
      */
-    public double getL() {
-        return L;
+    public double getLambda() {
+        return lambda;
     }
 
     /**
-     * @param L the L to set
+     * @param lambda the lambda to set
      */
-    public void setL(double L) {
-        this.L = L;
+    public void setLambda(double lambda) {
+        this.lambda = lambda;
     }
+
+    /**
+     * @return the r
+     */
+    public double getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    /**
+     * @return the phi
+     */
+    public double getPhi() {
+        return phi;
+    }
+
+    /**
+     * @param phi the phi to set
+     */
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    /**
+     * @return the h
+     */
+    public double getH() {
+        return h;
+    }
+
+    /**
+     * @param h the h to set
+     */
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    /**
+     * @return the a
+     */
+    public double getA() {
+        return a;
+    }
+
+    /**
+     * @param a the a to set
+     */
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    /**
+     * @return the v
+     */
+    public double getV() {
+        return v;
+    }
+
+    /**
+     * @param v the v to set
+     */
+    public void setV(double v) {
+        this.v = v;
+    }
+
+    /**
+     * @return the R0
+     */
+    public double getR0() {
+        return R0;
+    }
+
+    /**
+     * @param R0 the R0 to set
+     */
+    public void setR0(double R0) {
+        this.R0 = R0;
+    }
+   
+    
+   
+
 
 }
