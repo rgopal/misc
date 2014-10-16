06299209a1f6f522ba23d428ba4a106f217c6ad7
rgopal <rgopal@gopals.us>
2014-10-16 18:00:30 -0400

    Lines for satellite selection.

--- link/src/com/erudyo/satellite/MapView.java
+++ link/src/com/erudyo/satellite/MapView.java
@@ -22,6 +22,7 @@ import com.codename1.ui.Image;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -46,71 +47,130 @@ public class MapView extends View {
         this.unit = "xx";
     }
 
+    public void showSatellite(final Selection selection,
+            final MapComponent mc, String satName, final LinesLayer tXline,
+            final LinesLayer rXline) {
+
+        try {
+            Log.p("MapView: displaying satellite " + satName
+                    + " for " + selection.getBand() + " band.", Log.DEBUG);
+            Image blue_pin = Image.createImage("/blue_pin.png");
+            Image red_pin = Image.createImage("/red_pin.png");
+
+            Satellite satellite = Satellite.satelliteHash.get(satName);
+            if (satellite == null) {
+                Log.p("MapView: satellite is null " + satName, Log.DEBUG);
+            }
+
+            PointsLayer pl = new PointsLayer();
+            pl.setPointIcon(red_pin);
+
+            // Coord takes it in degrees.   Don't use true for projected
+            Coord c = new Coord(Math.toDegrees(satellite.getLatitude()),
+                    Math.toDegrees(satellite.getLongitude()));
+
+            final PointLayer p = new PointLayer(c, satellite.getName(), red_pin);
+
+            p.setDisplayName(false);   // it clutters
+            pl.addPoint(p);
+
+            pl.addActionListener(new ActionListener() {
+                // need to get PointLayer and not PointsLayer
+
+                public void actionPerformed(ActionEvent evt) {
+                    PointLayer pnew = (PointLayer) evt.getSource();
+
+                    // Mercator is the cylindrical projection.  Don't
+                    // know why this has to be called
+                    Coord m = Mercator.inverseMercator(pnew.getLatitude(),
+                            pnew.getLongitude());
+                    // get the point in this point layer to print 
+                    Boolean ans = Dialog.show("GEO_Satellite", pnew.getName() + " at Long|Lat "
+                            + Com.toDMS(Math.toRadians(m.getLongitude())) + "|"
+                            + Com.toDMS(Math.toRadians(m.getLatitude()))
+                            + "Select this satellite?", "Yes", "No");
+                    if (ans) {
+                        Satellite satellite = Satellite.satelliteHash.get(pnew.getName());
+                        if (satellite == null) {
+                            Log.p("Mapview: can't find satellite " + pnew.getName(), Log.DEBUG);
+                        } else {
+                            // change satellite and update lines
+                            Log.p("Mapview: selecting satellite " + pnew.getName(), Log.DEBUG);
+                            selection.setSatellite(satellite);
+                            mc.removeLayer(tXline);
+                            mc.removeLayer(rXline);
+                            showLines(selection, mc, tXline, rXline);
+
+                        }
+                    }
+
+                    Log.p("MapView: satellite " + p.getName()
+                            + " long | lat " + " "
+                            + m.getLongitude() + "|"
+                            + m.getLatitude(), Log.DEBUG);
+
+                }
+            });
+            mc.addLayer(pl);
+            // Google coordinatges are in degrees (no minutes, seconds)
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void showLines(final Selection selection, final MapComponent mc,
+            final LinesLayer tXline, final LinesLayer rXline) {
+
+        Coord tx, cSatellite, rx;
+
+        tx = new Coord(Math.toDegrees(selection.gettXterminal().getLatitude()),
+                Math.toDegrees(selection.gettXterminal().getLongitude()));
+
+        cSatellite = new Coord(Math.toDegrees(selection.getSatellite().getLatitude()),
+                Math.toDegrees(selection.getSatellite().getLongitude()));
+
+        tXline.addLineSegment(new Coord[]{tx, cSatellite});
+        tXline.lineColor(0xFF0000);
+        mc.addLayer(tXline);
+
+        rx = new Coord(Math.toDegrees(selection.getrXterminal().getLatitude()),
+                Math.toDegrees(selection.getrXterminal().getLongitude()));
+
+        cSatellite = new Coord(Math.toDegrees(selection.getSatellite().getLatitude()),
+                Math.toDegrees(selection.getSatellite().getLongitude()));
+
+        Log.p("MapView: drawing lines among " + rx + " " + cSatellite
+                + " " + tx, Log.DEBUG);
+
+        rXline.addLineSegment(new Coord[]{rx, cSatellite});
+        mc.addLayer(rXline);
+        rXline.lineColor(0x0000FF);  // Blue
+    }
+
     public Form createView(final Selection selection) {
+        
 
         try {
             Location loc = LocationManager.getLocationManager().getCurrentLocation();
             lastLocation = new Coord(loc.getLatitude(), loc.getLongtitude(), true);
 
+            final LinesLayer rXline = new LinesLayer();
+            final LinesLayer tXline = new LinesLayer();
+
             // this would not work if longPointerPress was overriden in MapComponent
             final MapComponent mc = new MapComponent(new GoogleMapsProvider("AIzaSyBEUsbb2NkrYxdQSG-kUgjZCoaLY0QhYmk"), loc, 5);
+
+            // now draw lines between terminals and satellite
+            showLines(selection, mc, tXline, rXline);
 
             // show all satellites on the map
             // showSatellites(selection, mc);
             for (String sat : selection.getBandSatellite().
                     get(selection.getBand())) {
-                try {
-                    Log.p("MapView: displaying satellite " + sat
-                            + " for " + selection.getBand() + " band.", Log.DEBUG);
-                    Image blue_pin = Image.createImage("/blue_pin.png");
-                    Image red_pin = Image.createImage("/red_pin.png");
-
-                    Satellite satellite = Satellite.satelliteHash.get(sat);
-                    if (satellite == null) {
-                        Log.p("MapView: satellite is null " + sat, Log.DEBUG);
-                    }
-
-                    PointsLayer pl = new PointsLayer();
-                    pl.setPointIcon(red_pin);
-
-                    // Coord takes it in degrees.   Don't use true for projected
-                    Coord c = new Coord(Math.toDegrees(satellite.getLatitude()),
-                            Math.toDegrees(satellite.getLongitude()));
-
-                    final PointLayer p = new PointLayer(c, satellite.getName(), red_pin);
-
-                    p.setDisplayName(false);   // it clutters
-                    pl.addPoint(p);
-
-                    pl.addActionListener(new ActionListener() {
-                        // need to get PointLayer and not PointsLayer
-
-                        public void actionPerformed(ActionEvent evt) {
-                            PointLayer pnew = (PointLayer) evt.getSource();
-
-                            // Mercator is the cylindrical projection.  Don't
-                            // know why this has to be called
-                            Coord m = Mercator.inverseMercator(pnew.getLatitude(),
-                                    pnew.getLongitude());
-                            // get the point in this point layer to print 
-                                Dialog.show("GEO_Satellite", pnew.getName() + " at Long|Lat " +
-                                      Com.toDMS(Math.toRadians(m.getLongitude())) + "|" +  
-                                        Com.toDMS(Math.toRadians(m.getLatitude())), "OK", null);
-                            Log.p("MapView: satellite " + p.getName()
-                                    + " long | lat " + " "
-                                    + m.getLongitude() + "|"
-                                    + m.getLatitude(), Log.DEBUG);
-
-                        }
-                    });
-                    mc.addLayer(pl);
-                    // Google coordinatges are in degrees (no minutes, seconds)
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+                showSatellite(selection, mc, sat, tXline, rXline);
             }
-
-            // now all terminals
+            // now display all terminal all terminals
             for (String term : selection.getBandTerminal().
                     get(selection.getBand())) {
                 try {
@@ -147,9 +207,9 @@ public class MapView extends View {
                             Coord m = Mercator.inverseMercator(pnew.getLatitude(),
                                     pnew.getLongitude());
                             // get the point in this point layer to print 
-                                Dialog.show("Terminal", pnew.getName() + " at Long|Lat " +
-                                      Com.toDMS(Math.toRadians(m.getLongitude())) + "|" +  
-                                        Com.toDMS(Math.toRadians(m.getLatitude())), "OK", null);
+                            Dialog.show("Terminal", pnew.getName() + " at Long|Lat "
+                                    + Com.toDMS(Math.toRadians(m.getLongitude())) + "|"
+                                    + Com.toDMS(Math.toRadians(m.getLatitude())), "OK", null);
                             Log.p("MapView: terminal " + p.getName()
                                     + " long | lat " + " "
                                     + m.getLongitude() + "|"
@@ -163,30 +223,6 @@ public class MapView extends View {
                     ex.printStackTrace();
                 }
             }
-
-            // now draw lines between terminals and satellite
-            Coord tx, sat, rx;
-            LinesLayer line = new LinesLayer();
-            tx = new Coord(Math.toDegrees(selection.gettXterminal().getLatitude()),
-                    Math.toDegrees(selection.gettXterminal().getLongitude()));
-
-            sat = new Coord(Math.toDegrees(selection.getSatellite().getLatitude()),
-                    Math.toDegrees(selection.getSatellite().getLongitude()));
-
-            line.addLineSegment(new Coord[]{tx, sat});
-            mc.addLayer(line);
-
-            rx = new Coord(Math.toDegrees(selection.getrXterminal().getLatitude()),
-                    Math.toDegrees(selection.getrXterminal().getLongitude()));
-
-            sat = new Coord(Math.toDegrees(selection.getSatellite().getLatitude()),
-                    Math.toDegrees(selection.getSatellite().getLongitude()));
-
-            Log.p("MapView: drawing lines among " + rx + " " + sat
-                    + " " + tx, Log.DEBUG);
-
-            line.addLineSegment(new Coord[]{rx, sat});
-            mc.addLayer(line);
 
             map = new Form(getName()) {
                 @Override
@@ -204,7 +240,7 @@ public class MapView extends View {
                         // dc.setProjected(true);  // WRONG
                         name = "T" + java.lang.String.valueOf((int) c.getLongitude())
                                 + String.valueOf((int) c.getLatitude());
-                        
+
                         final PointLayer p = new PointLayer(c, name, blue_pin);
 
                         // TODO - create a new terminal
@@ -227,7 +263,7 @@ public class MapView extends View {
                                 // make it back to WGS84 (add point had called fromWGS
                                 Coord m = Mercator.inverseMercator(pnew.getLatitude(),
                                         pnew.getLongitude());
-                          
+
                                 // get the point in this point layer to print 
                                 Log.p("MapView: new terminal current long | lat "
                                         + m.getLongitude() + "|" + m.getLatitude(), Log.DEBUG);
