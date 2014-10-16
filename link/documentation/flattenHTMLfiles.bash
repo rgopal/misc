010266cc6f3be264cb370046000cbc94d0e5b780
rgopal <>
2014-10-16 17:53:05 +0000

    create script to flatten MS word generated web htm files

--- link/documentation/flattenHTMLfiles.bash
+++ link/documentation/flattenHTMLfiles.bash
@@ -1,8 +1,23 @@
 #!/bin/bash
-FILES=`ls $1`
+# look for all htm files in the directory
+mkdir $1_flat
+cd $1
+FILES=`ls *htm`
 for f in $FILES
 do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  cat $1/$f
+  first=${f/\.htm/}
+  echo "Processing $first.htm file in directory $1..."
+
+  cat ${first}.htm | sed 's/src[[:space:]]*=[[:space:]]*'$first'\_files\//src='$first'_flat_/' > "../${1}_flat/${first}.htm"
+
+  
+  echo "moving to directory ${first}_files"
+  cd ${first}_files
+  for d in `ls *`
+  do
+     echo "Copying file $d in `pwd` to ../../$1_flat/${first}_flat_$d"
+     cp $d ../../${1}_flat/${first}_flat_$d
+
+  done
+  cd ..
 done
