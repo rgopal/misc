31e93970890b11291584697e2045a8d547e66a83
oumuo1 <>
2015-03-06 11:33:15 -0500

    added new record in Account and tested it.

--- central/grails-app/conf/BootStrap.groovy
+++ central/grails-app/conf/BootStrap.groovy
@@ -3,7 +3,7 @@ import central.Account
 class BootStrap {
     def init = { servletContext ->
 
-        def a1 = new Account(email:'a@b.com')
+        def a1 = new Account(email:'a@b.com', userName: 'Harry')
 
         if (!a1.save()){ a1.errors.allErrors.each {error ->
                 println "An error occured with a1: ${error}"
