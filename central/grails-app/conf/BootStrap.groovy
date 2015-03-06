c0a24d603a8eb7e7f3960df4eb6aac9bb68fe1d9
oumuo1 <>
2015-03-06 16:41:04 -0500

    now create 100 records for account using a loop.

--- central/grails-app/conf/BootStrap.groovy
+++ central/grails-app/conf/BootStrap.groovy
@@ -3,13 +3,16 @@ import central.Account
 class BootStrap {
     def init = { servletContext ->
 
-        def a1 = new Account(email:'a@b.com', userName: 'Harry')
+        // create 100 records with random names and emails (all unique)
+        for (int i=0; i<100; i++) {
+            def a1 = new Account(email: "aim${i}@b.com", userName: "${i} Harry")
 
-        if (!a1.save()){ a1.errors.allErrors.each {error ->
-                println "An error occured with a1: ${error}"
+            if (!a1.save()){ a1.errors.allErrors.each {error ->
+                    println "An error occured with a1: ${error}"
 
+                }
             }
-        }
+        } 
     }
    
     def destory = {
