fb9855051a25e951bd86c8896219a440bcbae311
oumuo1 <>
2015-03-06 11:29:30 -0500

    added new record in Account

--- central/grails-app/conf/BootStrap.groovy
+++ central/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,20 @@
-class BootStrap {
+import central.Account
 
+class BootStrap {
     def init = { servletContext ->
+
+        def a1 = new Account(email:'a@b.com')
+
+        if (!a1.save()){ a1.errors.allErrors.each {error ->
+                println "An error occured with a1: ${error}"
+
+            }
+        }
     }
-    def destroy = {
+   
+    def destory = {
+        
     }
+    
+    
 }
