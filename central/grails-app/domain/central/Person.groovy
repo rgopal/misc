62e37680078ee6eb03146bd4826cabb4183acb2d
oumuo1 <>
2015-03-31 12:31:18 -0400

    More person records.  Hindi also.  Expanded Hindi literals in messages.

--- central/grails-app/domain/central/Person.groovy
+++ central/grails-app/domain/central/Person.groovy
@@ -3,6 +3,7 @@ import central.Status
 import central.Country
 import central.Language
 import central.Email
+import central.Race
 
 class Person {
 
@@ -15,13 +16,7 @@ class Person {
         public String[] getCodes() { [ name() ] }
         public String getDefaultMessage() { name() }    // default is name itself
     }
-    enum Race implements org.springframework.context.MessageSourceResolvable {
  
-        WHITE, BLACK, HISPANIC, ASIAN, NATIVE_INDIAN, ASIAN_INDIAN
-        public Object[] getArguments() { [] as Object[] }
-        public String[] getCodes() { [ name() ] }
-        public String getDefaultMessage() { name() }    // default is name itself
-    }
    
     // class unique fields
      
@@ -29,8 +24,12 @@ class Person {
     Sex sex = Sex.UNKNOWN
     Date dateOfBirth = Date.parse('dd-MM-yyyy','01-01-1960')
     Race race = Race.WHITE
+    
+    String addressLine1
+    String addressLine2
     String city = 'Germantown'
     String state  = 'Maryland'
+    String zip = '20878'
     Country country = Country.USA
     Language preferredLanguage = Language.ENGLISH
 
@@ -41,13 +40,14 @@ class Person {
     Date dateCreated  
     Date lastUpdated 
     String comment
+    
     // will bring it back to composition, spent 2 days 3/30
     String homeEmail
     String workEmail
     String homePhone
     String workPhone
     String mobilePhone
-    String zip
+
    
     // associations
     static hasMany = [accounts:Account]
@@ -66,36 +66,39 @@ class Person {
      
         name (blank:false, size:2..64)
         sex ()
-        dateOfBirth(nullable:true)
         dateOfBirth(max: new Date(), min:Date.parse('dd-MM-yyyy','01-01-1901'), nullable:true)
         race (nullable:true)
         status ()
+        preferredLanguage(nullable:false)
+     
         homeEmail(nullable:true, email:true)
         workEmail(nullable:true, email:true)
         homePhone(nullable:true, matches: '\\d{3}\\-\\d{7}')
         workPhone(nullable:true, matches: '\\d{3}\\-\\d{7}')
         mobilePhone(nullable:true, matches: '\\d{3}\\-\\d{7}')
-        // validator takes 3 (1 is value, 2 obj, 3 error
-        // returns true or null (both good) or error messages
-        
+    
+        addressLine1(nullable:true)
+        addressLine2(nullable:true)
+        city (nullable:true)
+        state(nullable:true)
+        country (nullable:false)
         zip (nullable:true, size: 5..10, validator:{zip, obj ->
                 if (!zip) {
                     true
                 } else if (obj.country == Country.USA) {
-                    (zip ==~ /^(\d{5}-\d{4})|(\d{5})$/) ? true : false
+                    (zip ==~ /^(\d{5}-\d{4})|(\d{5})$/) ? true : false 
+                }
+                else if (obj.country == Country.INDIA) {
+                    (zip ==~ /^(\d{6}-\d{4})|(\d{6})$/) ? true : false
                 } else {
                     true
                 }
             })
-        
+        // validator takes 3 (1 is value, 2 obj, 3 error
+        // returns true or null (both good) or error messages
         
         accounts ()
-       
-        city (nullable:true)
-        state(nullable:true)
-        country (nullable:false)
-        preferredLanguage(nullable:false)
-     
+   
         dateCreated ()
         lastUpdated ()
         comment (nullable:true, maxSize:1000)
--- central/src/groovy/central/InitPerson.groovy
+++ central/src/groovy/central/InitPerson.groovy
@@ -8,6 +8,7 @@ package central
 import central.Person
 import central.Language
 import central.Email
+import central.Race
 
 /**
  *
@@ -20,19 +21,33 @@ class InitPerson {
             // this has Email embedded object which works here but GUI cannot save
             // The List GUI can display value saved here (but GUI destroys it)
             // TODO: fix the GUI for embedded
-            new Person(name: 'John Smith', sex:Person.Sex.MALE, race: Person.Race.WHITE, 
+            new Person(name: 'John Smith', sex:Person.Sex.MALE, race: Race.WHITE, 
                 dateOfBirth:Date.parse('dd-MM-yyyy','01-09-1960'), 
                 preferredLanguage:Language.ENGLISH,
-                homeEmail:new Email(email:'John.Smith@gmail.com', provider:'google')
+                // embedded does not work 
+                // homeEmail:new Email(email:'John.Smith@gmail.com', provider:'google')
+                homeEmail:'john.smith@gmail.com'
             ),
            
-            new Person(name: 'Mike Johns', sex:Person.Sex.MALE, race: Person.Race.WHITE, 
+            new Person(name: 'Mike Johns', sex:Person.Sex.MALE, race: Race.WHITE, 
                 dateOfBirth:Date.parse('dd-MM-yyyy','0-09-1970'),
-                preferredLanguage:Language.ENGLISH).addToAccounts( new Account(
-                    email:'a@b.com', userName : 'John.Smith')
-            ),
-            new Person(name: 'Jane Fields', sex:Person.Sex.FEMALE, race: Person.Race.BLACK, 
-                dateOfBirth:Date.parse('dd-MM-yyyy','02-09-1980'),preferredLanguage:Language.ENGLISH)
+                preferredLanguage:Language.ENGLISH,
+                homeEmail:'Mike.Johns@gmail.com' ).addToAccounts( new Account(
+                    email:'johnsmith@facebook.com', userName : 'John Smith again')
+            ), 
+            
+            new Person(name: 'Jane Fields', sex:Person.Sex.FEMALE, race: Race.BLACK, 
+                dateOfBirth:Date.parse('dd-MM-yyyy','02-09-1980'),preferredLanguage:Language.ENGLISH),
+            
+            new Person(name: 'Ram Pandey', sex:Person.Sex.MALE, race: Race.ASIAN_INDIAN, 
+                country:Country.INDIA,
+                zip:'160031', city:'Ahmedabad', state: 'Gujarat',
+                dateOfBirth:Date.parse('dd-MM-yyyy','02-09-1986'),preferredLanguage:Language.ENGLISH),
+            
+            new Person(name: 'राहुल नंदा', sex:Person.Sex.MALE, race: Race.ASIAN_INDIAN, 
+                country:Country.INDIA,
+                zip:'260014', city:'लखनऊ', state: 'उत्तर प्रदेश',
+                dateOfBirth:Date.parse('dd-MM-yyyy','02-09-1992'),preferredLanguage:Language.HINDI)
         ]
        
         for (person in persons) {
