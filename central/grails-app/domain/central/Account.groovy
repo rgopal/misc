c82c811f554f913cd3920393fbb7a18c478dcace
oumuo1 <>
2015-04-01 12:53:51 -0400

    Sequence (in Account) update works with using a formal association property (person)

--- central/grails-app/domain/central/Account.groovy
+++ central/grails-app/domain/central/Account.groovy
@@ -1,11 +1,16 @@
 package central
 import central.Person
+import org.apache.commons.logging.LogFactory
+import groovy.util.logging.Log4j
 
+@Log4j
 class Account {
+        
+    // def log = LogFactory.getLog(this)
     String name
     Long sequence
     Boolean main = false
-    Person owner
+    // Person owner (does not work well with sequence updates)
     String email
     Date dateCreated
     Date lastUpdated
@@ -17,9 +22,9 @@ class Account {
     // only one Person can own an Account (owner) with cascaded deletes
     // without belongsTo, an account can be associated with multiple persons
     // akin to a lookup field (instead of true master-detail
-    static belongsTo = Person
+    static belongsTo = [person: Person]
     static constraints = {
-        owner()
+        // named association so not needed owner()
         sequence (nullable:true)
         name (nullable:true)
         main()
@@ -36,33 +41,29 @@ class Account {
      */
     def beforeInsert() {
         if (!sequence) {
-            if (!owner.id) {
+                        
+            def count = Account.createCriteria().count {
+                person {
+                    eq ('id', person.id)
+                }
+            }
+           
+            if (count == 0) {
                 sequence = 1
             } else {
                 
-                def count = Account.createCriteria().count {
-                    eq ('owner', owner)
-                   /* owner {
-                        eq('id', owner?.id)
-                    } */
-                }
-                println "count = ${count}"
-                if (count == 0) {
-                    sequence = 1
-                } else {
-                
-                    sequence = Account.createCriteria().get {
-                        projections {
-                            max('sequence')
-                        }
-                        owner {
-                            eq('id', owner?.id)
-                        }
-                    } + 1
-                }
-                println ("beforeInsert: owner.id ${owner.id} sequence = ${sequence}")
-            
+                sequence = Account.createCriteria().get {
+                    projections {
+                        max('sequence')
+                    }
+                    person {
+                        eq ('id', person.id)
+                    }
+                } + 1
             }
+            // log.debug ("beforeInsert: person.id ${person.id} sequence = ${sequence}")
+            
         }
+        
     }
 }
