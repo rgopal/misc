0d1350cac71f26296b7909e140f4760c1a0a9851
oumuo1 <>
2015-04-22 10:42:39 -0400

    Added security ACL.

--- central/grails-app/conf/BuildConfig.groovy
+++ central/grails-app/conf/BuildConfig.groovy
@@ -73,6 +73,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.11.1"
         compile ':spring-security-core:2.0-RC4'
         compile ":spring-security-ui:1.0-RC2"
+        compile ":spring-security-acl:2.0-RC2"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
